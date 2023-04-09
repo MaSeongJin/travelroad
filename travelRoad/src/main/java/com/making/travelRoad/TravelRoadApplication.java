7e27e798f3cdc5e2adbb12ff9bc7a000185ceb6b
MaSeongJin <making1104@naver.com>
2023-04-09 22:29:47 +0900

    프로젝트 생성 및 세팅

---
+++ travelRoad/src/main/java/com/making/travelRoad/TravelRoadApplication.java
@@ -0,0 +1,13 @@
+package com.making.travelRoad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelRoadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelRoadApplication.class, args);
+	}
+
+}
---
+++ travelRoad/src/test/java/com/making/travelRoad/TravelRoadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.making.travelRoad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelRoadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
